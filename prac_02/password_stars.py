0b8ef00ef86b43c68a875e95f141323b1f816489
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-06-01 21:15:44 +0800

    Add password check program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,10 +1,17 @@
 MIN_LENGTH = 8
 
 def main():
+    password = get_password()
+    print_asterisks(password)
+
+def get_password():
     password = input("Enter password: ")
     while len(password) < MIN_LENGTH:
         print("Password too short")
         password = input("Enter password: ")
+    return password
+
+def print_asterisks(password):
     print("*" * len(password))
 
 main()
