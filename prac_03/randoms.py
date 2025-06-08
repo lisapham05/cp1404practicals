645303028a71e872a4d7ab133364d1b6fec7da64
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-06-08 14:26:46 +0800

    random.randrange

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -9,5 +9,15 @@ Largest possible: 20
 Randint(a,b) includes both endpoints a and b
 """
 
+print(random.randrange(3,10,2))  # line 2
+"""
+Line 2:
+I saw 5, 3, 7, 9
+Smallest possible: 3
+Largest possible: 9
+Line 2 could not produce a 4
+randrange(3,10,2) has step = 2 from start, so it gives values 3,5,7,9
+"""
+
 
 
