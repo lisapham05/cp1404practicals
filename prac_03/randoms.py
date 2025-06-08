fdb1e5187b3e967dda1fc9649874efac04997c24
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-06-08 14:26:22 +0800

    random.randint

---
+++ prac_03/randoms.py
@@ -0,0 +1,13 @@
+import random
+
+print(random.randint(5,20))  # line 1
+"""
+Line 1:
+I saw 12, 11, 7, 10,...
+Smallest possible: 5
+Largest possible: 20
+Randint(a,b) includes both endpoints a and b
+"""
+
+
+
