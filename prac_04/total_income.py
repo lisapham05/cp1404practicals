a36f802706ced3d9a0eeb2bdefd17ad29339dfa2
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-06-11 15:38:58 +0800

    Fix and adjust to make it right

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -1,40 +1,30 @@
 """
-Sample output:
-How many months? 5
-
-Enter income for month 1: 120
-Enter income for month 2: 245.4
-Enter income for month 3: 900
-Enter income for month 4: 1205.56
-Enter income for month 5: 12.35
-
-Income Report
--------------
-Month  1 - Income: $    120.00         Total: $    120.00
-Month  2 - Income: $    245.40         Total: $    365.40
-Month  3 - Income: $    900.00         Total: $   1265.40
-Month  4 - Income: $   1205.56         Total: $   2470.96
-Month  5 - Income: $     12.35         Total: $   2483.31
+CP1404/CP5632 Practical
+Starter code for cumulative total income program
 """
 
 def main():
-    number_of_months = int(input("How many months? "))
+    """Display income report for incomes over a given number of months."""
     incomes = []
+    months = int(input("How many months? "))
 
-    for month in range(1, number_of_months + 1):
-        income = float(input(f"Enter income for month {month}: "))
+
+    for month in range(1, months + 1):
+        income = float(input("Enter income for month " + str(month) + ": "))
         incomes.append(income)
 
-    print_report(incomes)
+    print_report(incomes, months)
+
+def print_report(incomes, months):
+    """Prints an income report showing each month 's income and total"""
+    print("Income Report\n-------------")
+
+    total = 0
+    for month in range(1, months + 1):
+        income = incomes[month - 1]
+        total += income
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
 
 
-def print_report(incomes):
-    print("\nIncome Report")
-    print("-------------")
-
-    cumulative_total = 0
-    for month_index, income in enumerate(incomes):
-        cumulative_total += income
-        print(f"Month {month_index + 1:2} - Income: ${income:10.2f}         Total: ${cumulative_total:10.2f}")
 
 main()
