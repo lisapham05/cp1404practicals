36847d7ec755afdf418d1bca341671a1d2b4cd9b
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-06-11 17:30:03 +0800

    checking access

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -20,7 +20,7 @@ main()
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 
-user_input = input("Enter your name: ")
+user_input = input("Enter your name:")
 if user_input in usernames:
     print("Access granted")
 else:
