2cfb76d58b91d7e3395d8959878d468466f337ec
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-06-11 15:57:30 +0800

    add display_subjects and adjust the code

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,22 +8,22 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = load_data()
-    print(data)
+    display_subjects(data)
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
-    input_file = open(FILENAME)
-    for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
-    input_file.close()
+    subject_data = []
+    with open(FILENAME) as input_file:
+        for line in input_file:
+            line = line.strip()
+            parts = line.split(',')
+            parts[2] = int(parts[2])
+            subject_data.append(parts)
+    return subject_data
 
+def display_subjects(data):
+    for subjects, lecturer, students in data:
+        print(f"{subject} is taught by {lecturer} and has {students} students")
 
 main()
