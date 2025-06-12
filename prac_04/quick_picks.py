7594dc29f11a626ad8a84016f24290bc7184ab1e
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-06-12 16:01:15 +0800

    main function

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -4,3 +4,8 @@ NUMBERS_PER_PICK = 6
 MIN_NUMBER = 1
 MAX_NUMBER = 45
 
+def main():
+    number_of_picks = int(input("How many quick picks? "))
+    for i in range(number_of_picks):
+        quick_pick = generate_quick_pick()
+
