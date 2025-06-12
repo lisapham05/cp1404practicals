eaa7dc05a5ab8ffba7e95c366202367148a7b39f
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-06-12 16:08:29 +0800

    function generate_quick_pick

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -9,3 +9,14 @@ def main():
     for i in range(number_of_picks):
         quick_pick = generate_quick_pick()
 
+def generate_quick_pick():
+    numbers = []
+    while len(numbers) < NUMBERS_PER_PICK:
+        number = random.randint(MIN_NUMBER, MAX_NUMBER)
+        if number not in numbers:
+            numbers. append(number)
+    numbers.sort()
+    return numbers
+
+
+main()
