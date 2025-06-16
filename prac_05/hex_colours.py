cd29dbd20c1c26c656864b5d9563f9baaa82c02e
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-06-16 14:51:44 +0800

    Create the constant dictionary of colour names and hex codes

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,12 @@
+COLOUR_NAME_TO_HEX = {
+    "aliceblue": "#f0f8ff",
+    "amber": "#ffbf00",
+    "azure": "#f0ffff",
+    "beige": "#f5f5dc",
+    "baby blue": "#89cff0",
+    "baby pink": "#f4c2c2",
+    "bittersweet": "#fe6f5e",
+    "bubble gum": "	#ffc1cc",
+    "cambridge blue": "#a3c1ad",
+    "camel": "#c19a6b"
+}
