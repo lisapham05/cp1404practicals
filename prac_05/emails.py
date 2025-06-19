f3a0dc01f1764b7f21fba5cbd94d417015db4cc2
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-06-19 14:49:46 +0800

    Name correction and store email-name pair in dictionary

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -10,8 +10,15 @@ def main():
     while email != "":
         email = input("Email: ").strip()
 
-default_name = get_name_from_email(email)
-confirmation = input(f"Is your name {default_name}? (Y/n) ").strip().lower()
+    default_name = get_name_from_email(email)
+    confirmation = input(f"Is your name {default_name}? (Y/n) ").strip().lower()
+
+    if confirmation == "" or confirmation == "y":
+        name = default_name
+    else:
+        name = input("Name: "). strip()
+
+    email_to_name[email] = name
 
 
 
