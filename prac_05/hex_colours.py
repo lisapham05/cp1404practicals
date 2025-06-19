6f4a9f00cd6d80448ae93a9dc8123c46e70fda13
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-06-19 14:00:36 +0800

    create constants COLOUR_NAME_TO_HEX

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,12 @@
+COLOUR_NAME_TO_HEX = {
+    "ALICEBLUE":"#f0f8ff",
+    "ANTIQUEWHITE":"#faebd7",
+    "BABYBLUE":"#89cff0",
+    "BABYPINK":"#f4c2c2",
+    "BANANAMANIA":"#fae7b5",
+    "BOYSENBERRY":"#873260",
+    "BRONZE":"#cd7f32",
+    "BUFF":"#f0dc82",
+    "CAMEL":"#c19a6b",
+    "CELESTE":"#b2ffff"
+}
