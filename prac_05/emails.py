588780446c5cb9387edfb52c9f71942fb1f2407f
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-06-19 14:43:44 +0800

    main function for input and checking validity of input, and email collection

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -4,5 +4,11 @@ def get_name_from_email(email):
     parts = prefix.replace('.', ' ').replace('_', ' ').split()
     return ' '.join(parts).title()
 
+def main():
+    email_to_name = {}
+    email = input("Email: ").strip()
+    while email != "":
+        email = input("Email: ").strip()
+
 
 
