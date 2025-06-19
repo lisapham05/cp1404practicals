1d8579e2b5154114999db0302277da15d80aeb70
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-06-19 15:28:47 +0800

    read csv data with utf-8 handling

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -5,3 +5,9 @@ Actual time:
 """
 
 FILENAME = 'wimbledon.csv'
+
+def read_wimbledon_data(filename):
+    with open(FILENAME, 'r', encoding='utf-8-sig') as in_file:
+       data = [line.strip().split(',') for line in in_file]
+    return data
+
