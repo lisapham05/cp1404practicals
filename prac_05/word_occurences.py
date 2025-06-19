8c30e85d58fcdb5e07122ef3781d1f2305f82540
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-06-19 14:11:43 +0800

    add word count program by for loops and if, else

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -2,3 +2,8 @@ text = input("Text: ").strip().lower()
 words = text.split()
 
 word_to_count = {}
+for word in words:
+    if word in word_to_count:
+        word_to_count[word] += 1
+    else:
+        word_to_count[word] = 1
