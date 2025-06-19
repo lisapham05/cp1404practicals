dea3aab2d66396d941fcf91e9c4b016765d5c4fd
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-06-19 14:14:11 +0800

    Implement sorted and aligned output with for loop

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -9,3 +9,6 @@ for word in words:
         word_to_count[word] = 1
 
 max_word_length = max(len(word) for word in word_to_count)
+
+for word in sorted(word_to_count):
+    print(f"{word:{max_word_length}} : {word_to_count[word]}")
