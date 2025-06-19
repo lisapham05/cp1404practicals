707465ddf6b2af881ed061623686fbdc240c0a06
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-06-19 13:34:37 +0800

    copy the code from state_names.py

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,26 +5,15 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {
-    "QLD": "Queensland",
-    "NSW": "New South Wales",
-    "NT": "Northern Territory",
-    "WA": "Western Australia",
-    "ACT": "Australian Capital Territory",
-    "VIC": "Victoria",
-    "TAS": "Tasmania",
-    "SA": "South Australia"
-    }
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
+print(CODE_TO_NAME)
 
-for code in CODE_TO_NAME:
-    print(code, end=" ")
-print()
-
-state_code = input("Enter short state: ").upper()
+state_code = input("Enter short state: ")
 while state_code != "":
     if state_code in CODE_TO_NAME:
         print(state_code, "is", CODE_TO_NAME[state_code])
     else:
         print("Invalid short state")
-    state_code = input("Enter short state: ").upper()
+    state_code = input("Enter short state: ")
 
