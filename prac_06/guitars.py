a2d78947a55e26c6ae01747ce2f66e60cbe1413a
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-06 20:21:23 +0800

    Print guitars list with for loop

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -7,3 +7,10 @@ def main():
 
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.90))
+
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+main()
