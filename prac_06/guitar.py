9b85f0554af4e1ecbae6107d7e0cdaa86a06811f
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-06 19:44:56 +0800

    Create class Guitar and add init method to represent a guitar with name, year and cost

---
+++ prac_06/guitar.py
@@ -0,0 +1,6 @@
+class Guitar:
+    """Represent a guitar with name, year of manufacturer, cost """
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
