c5f21250a211f13fdbc94f80528e5aff05905500
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-06 19:13:42 +0800

    Copy and paste the code from github

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -8,3 +8,9 @@ Actual time:
 
 from programming_language import ProgrammingLanguage
 
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    print(python)
