846f910d4fce90f62ec409b690c310932ef83ac2
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-06 20:13:26 +0800

    Add test 2

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -9,3 +9,9 @@ def test_guitar():
     print(f"{guitar_1.name} get_age() - Expected {age_1}. Got {guitar_1.get_age()}")
     print(f"{guitar_1.name} is_vintage() - Expected True. Got {guitar_1.is_vintage()}")
 
+    guitar_2 = Guitar("Another Guitar", 2013, 1000.00)
+    age_2 = current_year - 2013
+    print(f"{guitar_2.name} get_age() - Expected {age_2}. Got {guitar_2.get_age()}")
+    print(f"{guitar_2.name} is_vintage() - Expected False. Got {guitar_2.is_vintage()}")
+
+test_guitar()
