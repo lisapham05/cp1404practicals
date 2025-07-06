29c72fdfedcbc3ec44a28946435de35907f0bab9
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-06 19:10:52 +0800

    Add doctrings for methods and estimated and actual time of doing this code

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -2,19 +2,24 @@
 Program: programming_language.py
 
 Estimated time: 30 minutes
-Actual time:
+Actual time: 17 minutes
 
 """
 
 class ProgrammingLanguage:
+    """Represents a programming language with characteristics"""
+
     def __init__(self, name, typing, reflection, year):
+        """ Initialize a ProgrammingLanguage object """
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self):
+        """ Determine if the programming language is dynamically typed or not """
         return self.typing  == "dynamic"
 
     def __str__(self):
+        """ Return a string representation of the ProgrammingLanguage object """
         return f"{self.name}, {self.typing} Typing, Reflection = {self.reflection}, first appeared in {self.year}"
