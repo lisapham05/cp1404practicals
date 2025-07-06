17883ef1586c0e46fec586d928a4d3d7017a3429
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-06 20:13:09 +0800

    Add test 1

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,2 +1,11 @@
 from guitar import Guitar
 
+def test_guitar():
+    """Test Guitar class"""
+    current_year = 2025
+
+    guitar_1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    age_1 = current_year - 1922
+    print(f"{guitar_1.name} get_age() - Expected {age_1}. Got {guitar_1.get_age()}")
+    print(f"{guitar_1.name} is_vintage() - Expected True. Got {guitar_1.is_vintage()}")
+
