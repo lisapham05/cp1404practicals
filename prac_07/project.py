8f5de5b8ec0f8de5cb4b41dc21a8042b2974caf7
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-10 16:34:11 +0800

    Import datetime and add __init__ in Project class

--- prac_07/project.py
+++ prac_07/project.py
@@ -8,9 +8,17 @@ Actual time:
 from datetime import datetime
 
 class Project:
+    """To represent a project with a name, start date, priority, cost estimate, and completion percentage."""
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        """ Initialize Project"""
         self.name = name
         self.start_date = datetime.strptime(start_date, "%d/%m/%Y").date()
         self.priority = int(priority)
         self.cost_estimate = float(cost_estimate)
         self.completion_percentage = int(completion_percentage)
+
+    def __str__(self):
+        """Return string representation"""
+        return (f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, "
+                f"priority {self.priority}, estimate: ${self.cost_estimate:.2f}, "
+                f"completion: {self.completion_percentage}%")
