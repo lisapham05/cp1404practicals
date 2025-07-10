c3fff6f8eaf1a517627676f21618b266ca50b342
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-10 15:15:03 +0800

    Update language with pointer arithmetic

--- prac_07/language_file_reader.py
+++ prac_07/language_file_reader.py
@@ -29,7 +29,7 @@ def main():
         pointer_arithmetic = parts[4] == "Yes"
         # Construct a ProgrammingLanguage object using the elements
         # year should be an int
-        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]))
+        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]), pointer_arithmetic)
         # Add the language we've just constructed to the list
         languages.append(language)
     # Close the file as soon as we've finished reading it
