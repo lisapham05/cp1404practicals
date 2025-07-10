8e10a4fec1f6a2fb7291134824919782f2333aa3
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-10 16:10:55 +0800

    copy guitar.py code

---
+++ prac_07/guitar.py
@@ -0,0 +1,20 @@
+class Guitar:
+    """Represent a guitar with name, year of manufacturer, cost """
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return string representation of guitar """
+        return f"{self.name} ({self.year}): ${self.cost:.2f}"
+
+    def get_age(self):
+        """Return how old the guitar is in years"""
+        current_year = 2025
+        return current_year - self.year
+
+    def is_vintage(self):
+        """Determine if the guitar is over 50 years or not"""
+        return self.get_age() >= 50
+
