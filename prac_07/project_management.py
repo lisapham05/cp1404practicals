eaf50d9ebb53a1b1c7f221536bcef9c406407e58
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-13 03:08:11 +0800

    Add print_menu function and display it in main function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -24,14 +24,14 @@ def main():
 
 
 def print_menu():
-    print("""
-    - (L)oad projects
-    - (S)ave projects
-    - (D)isplay projects
-    - (F)ilter projects by date"
-    - (A)dd new project"
-    - (U)pdate project"
-    - (Q)uit""")
+    "Print menu choices"
+    print("- (L)oad projects"
+    "\n- (S)ave projects"
+    "\n- (D)isplay projects"
+    "\n- (F)ilter projects by date"
+    "\n- (A)dd new project"
+    "\n- (U)pdate project"
+    "\n- (Q)uit")
 
 def load_projects(filename):
     """Load projects from file"""
