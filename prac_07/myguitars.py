9d3785c2b8467dbff7ea07720e7398824f3a63d0
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-13 19:42:35 +0800

    Main program with the task in the program

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -2,3 +2,21 @@ import csv
 from guitar import Guitar
 
 FILENAME = "guitars.csv"
+
+def main():
+    """Main program to display guitar list."""
+    guitars = load_guitars(FILENAME)
+    print("These are the guitars loaded from file:")
+    display_guitars(guitars)
+
+    guitars.sort()    #Sorting
+    print("\nGuitars sorted by year:")
+    display_guitars(guitars)
+
+    add_new_guitars(guitars)     #Add new guitar
+    guitars.sort()               #Sort again
+    print("\nFinal list of guitars (sorted):")
+    display_guitars(guitars)
+
+    save_guitars(FILENAME, guitars)
+    print(f"\nGuitars have been saved to {FILENAME}.")
