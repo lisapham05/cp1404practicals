f46b3afd3b982d1aa2c436dd8d5657ad4ce2f927
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-17 21:09:13 +0800

    Add build method to load the UI and return the root widget

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -3,4 +3,13 @@ from kivy.lang import Builder
 from kivy.properties import StringProperty
 
 class SquaringApp(App):
+    """Main application class for squaring numbers with Kivy."""
     result = StringProperty('')
+
+    def build(self):
+        """Load the UI and return the root widget"""
+        self.title = "Squaring Numbers"
+        self.root = Builder.load_file('squaring.kv')
+        return self.root
+
+
