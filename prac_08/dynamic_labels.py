5bf0057b5b16f192303b0b32c8e13e5e1c8d88f0
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-17 21:03:31 +0800

    Add box_layout padding and spacing

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -6,7 +6,7 @@ class DynamicLabelsApp(App):
     def build(self):
         """ Kivy for the Dynamic Labels App. """
         self.root = Builder.load_file('dynamic_labels.kv')
-        self.names = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace"]
+        self.names = ["Alice", "Bob", "Charlie", "David"]
 
         for name in self.names:
             label = Label(text=name, font_size='20sp', size_hint_y=None, height=40)
