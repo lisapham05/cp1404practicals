0cbe1321cf53c1dd722eae48bebf8c0786dc78c7
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-17 19:45:12 +0800

    Add the main method to build kv app

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -2,3 +2,12 @@ from kivy.app import App
 from kivy.lang import Builder
 
 MILES_TO_KM_MULTIPLIER = 1.60934
+
+class ConvertMilesKilometers(App):
+    """ Kivy BoxLayout widget for the miles to kilometres converter. """
+    def build(self):
+        """ Build kv app from kv file """
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('convert_m_km_solution.kv')
+        return self.root
+
