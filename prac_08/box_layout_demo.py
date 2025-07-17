fd3a53ec4652dd9f01063f8c6849d6b14a8de54b
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-17 19:30:56 +0800

    Add handle_greet method in BoxLayout class

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -4,9 +4,21 @@ from kivy.lang import Builder
 
 class BoxLayoutDemo(App):
     def build(self):
+        """ Load and return the Kivy interface layout defined in the 'box_layout.kv' file. """
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
+
+    def handle_greet(self):
+        """
+        Event handler for the 'Greet' button.
+        Retrieves the text from the TextInput, prints 'greet' to the console,
+        and updates the Label to greet the user by name.
+        """
+        print('greet')
+        name = self.root.ids.input_name.text
+        self.root.ids.output_label.text = f"Hello {name}"
+
 
 
 BoxLayoutDemo().run()
