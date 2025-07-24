01af3535fd9bd90656e391a50cf7a1f407cbe576
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-24 17:43:23 +0800

    Add __str__ to return a string

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -15,3 +15,6 @@ class SilverServiceTaxi(Taxi):
         self.fanciness = fanciness
         self.price_per_km *= self.fanciness
 
+    def __str__(self):
+        """Return a string like a Taxi but with flagfall."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
