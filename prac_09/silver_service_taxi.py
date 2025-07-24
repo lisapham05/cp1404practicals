b6e71dd76946a12b6b4dc3fabe1f0e0f2f2d32d1
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-24 17:42:59 +0800

    Add __init__ method

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -9,3 +9,9 @@ class SilverServiceTaxi(Taxi):
     """Specialised version of a Taxi that includes a flagfall and fanciness multiplier."""
     flagfall = 4.50
 
+    def __init__(self, name, fuel, fanciness: float):
+        """Initialise a SilverServiceTaxi instance, based on parent class Taxi."""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= self.fanciness
+
