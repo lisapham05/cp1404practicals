eea60a2fb30f18271e765bbbc05ae3a1325fefd1
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-24 17:24:49 +0800

    add drive method that drive the car only if a random number is less than its reliability

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -13,3 +13,12 @@ class UnreliableCar(Car):
         """Initialise an UnreliableCar instance, based on parent class Car."""
         super().__init__(name, fuel)
         self.reliability = reliability
+
+    def drive(self, distance):
+        """Drive the car only if a random number is less than its reliability."""
+        random_number = randint(0, 100)
+        if random_number < self.reliability:
+            distance_driven = super().drive(distance)
+        else:
+            distance_driven = 0
+        return distance_driven
