a65d35682b2fe1e739da4c83f2dfc173218dcdc0
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-24 18:07:18 +0800

    create Band class that has __init__ method and __str__ method to initialise and return  string

---
+++ prac_09/band.py
@@ -0,0 +1,16 @@
+"""
+Band class
+"""
+
+class Band:
+    """Represent a Band object, which consists of a collection of musicians."""
+
+    def __init__(self, name=""):
+        """Initialise a Band instance."""
+        self.name = name
+        self.musicians = []
+
+    def __str__(self):
+        """Return a string representation of a Band, including its musicians."""
+        musician_details = ", ".join(str(musician) for musician in self.musicians)
+        return f"{self.name} ({musician_details})"
