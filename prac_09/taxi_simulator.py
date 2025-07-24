cd8ff4bea8699f583f47ab46fa3123ad1667d960
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-24 17:54:01 +0800

    Add variables: total_bill, taxis, current_taxi and print MENU

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -11,3 +11,13 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 
 def main():
     """A taxi simulator program that uses Taxi and SilverServiceTaxi classes."""
+
+    total_bill = 0
+    taxis = [Taxi("Prius", 100),
+             SilverServiceTaxi("Limo", 100, 2),
+             SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
+
+    print("Let's drive!")
+    print(MENU)
+    choice = input(">>> ").lower()
