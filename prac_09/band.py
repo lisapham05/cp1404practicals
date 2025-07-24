1e1490c2e83c3dad6cd950c604c0f7c5d6e08262
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-24 18:07:39 +0800

    Add add method to add musician to the band

--- prac_09/band.py
+++ prac_09/band.py
@@ -14,3 +14,10 @@ class Band:
         """Return a string representation of a Band, including its musicians."""
         musician_details = ", ".join(str(musician) for musician in self.musicians)
         return f"{self.name} ({musician_details})"
+
+
+    def add(self, musician: Musician):
+        """Add a musician to the band."""
+        self.musicians.append(musician)
+
+
