860138e34be27c569fa507dbd81ad364cceccf33
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-24 17:44:07 +0800

    Add get_fare method to return the price for taxi trip including flagfall

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -18,3 +18,7 @@ class SilverServiceTaxi(Taxi):
     def __str__(self):
         """Return a string like a Taxi but with flagfall."""
         return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
+
+    def get_fare(self):
+        """Return the price for the taxi trip including flagfall."""
+        return super().get_fare() + self.flagfall
