333322136922853828025e3f16f7b016fe951c15
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-24 17:46:21 +0800

    Import SilverServiceTaxi and create main function, then create object with fanciness of 2

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,10 @@
+"""
+Test SilverServiceTaxi
+"""
+
+from silver_service_taxi import SilverServiceTaxi
+
+def main():
+    """Test the SilverServiceTaxi class."""
+    hummer_taxi = SilverServiceTaxi("Hummer", 200, 2)
+    print(hummer_taxi)
