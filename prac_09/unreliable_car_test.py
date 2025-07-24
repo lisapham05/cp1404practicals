b4d206ee4008523958a90efabb81c364aa49a9d6
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-24 17:26:46 +0800

    Add reliable_car and unreliable car objects

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -1,2 +1,8 @@
 from unreliable_car import UnreliableCar
 
+
+def main():
+    """Test the UnreliableCar class."""
+    # Create instances of UnreliableCar
+    reliable_car = UnreliableCar("Mostly Good", 100, 90)
+    unreliable_car = UnreliableCar("Dodgy", 100, 9)
