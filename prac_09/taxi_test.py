e9ee550a80266ea5454e079b31e264636b26a1a0
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-24 17:08:58 +0800

    create taxi new object

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -1,2 +1,16 @@
 from taxi import Taxi
 
+"""
+CP1404/CP5632 Practical
+Test Taxi class
+"""
+from taxi import Taxi
+
+def main():
+    """Test the Taxi class."""
+    PRICE_PER_KM = 1.23
+    UNITS_OF_FUEL = 100
+
+    my_taxi = Taxi("Prius 1", UNITS_OF_FUEL, PRICE_PER_KM)
+
+
