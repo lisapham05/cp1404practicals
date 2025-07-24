49db98d5ee23f7b4ddf83e742cfce5fa947e7ac3
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-24 16:51:48 +0800

    delete price_per_km in init method

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -10,10 +10,9 @@ PRICE_PER_KM = 1.23
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
 
-    def __init__(self, name, fuel, price_per_km):
+    def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
-        self.price_per_km = price_per_km
         self.current_fare_distance = 0
 
     def __str__(self):
@@ -33,5 +32,3 @@ class Taxi(Car):
         distance_driven = super().drive(distance)
         self.current_fare_distance += distance_driven
         return distance_driven
-
-
