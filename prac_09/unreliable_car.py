9dea3234508cc7f8b0cf69b46ff3416fbd594207
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-24 17:23:40 +0800

    Add __init__ method to initialise UnreliableCar

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -6,4 +6,10 @@ UnreliableCar Class
 from car import Car
 from random import randint
 
+class UnreliableCar(Car):
+    """A specialised version of a Car that is not always reliable."""
 
+    def __init__(self, name, fuel, reliability: float):
+        """Initialise an UnreliableCar instance, based on parent class Car."""
+        super().__init__(name, fuel)
+        self.reliability = reliability
