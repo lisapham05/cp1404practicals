4ca59633353ed4407070f66ee231e0627f9cc4ec
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-24 17:19:15 +0800

    change to price_per_km instead of uppercase

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -7,7 +7,7 @@ from car import Car
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
 
-    PRICE_PER_KM = 1.23
+    price_per_km = 1.23
 
     def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car."""
@@ -16,11 +16,11 @@ class Taxi(Car):
 
     def __str__(self):
         """Return a string like a Car but with current fare distance."""
-        return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.PRICE_PER_KM:.2f}/km"
+        return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km"
 
     def get_fare(self):
         """Return the price for the taxi trip."""
-        return round(self.PRICE_PER_KM * self.current_fare_distance, 1)
+        return round(self.price_per_km * self.current_fare_distance, 1)
 
     def start_fare(self):
         """Begin a new fare."""
