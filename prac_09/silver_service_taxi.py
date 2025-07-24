c920f3b831244013a9a94cd6d87628f6c1216df9
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-24 17:42:34 +0800

    create SilverServiceTaxi

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,11 @@
+"""
+CP1404/CP5632 Practical 09
+SilverServiceTaxi class
+"""
+
+from taxi import Taxi
+
+class SilverServiceTaxi(Taxi):
+    """Specialised version of a Taxi that includes a flagfall and fanciness multiplier."""
+    flagfall = 4.50
+
