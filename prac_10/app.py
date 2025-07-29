ada20e23e5e27ef0945d852954bf6c3edc2b19be
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-29 21:20:06 +0800

    add greet function

--- prac_10/app.py
+++ prac_10/app.py
@@ -7,5 +7,13 @@ def hello_world():
     # Now returns an HTML h1 tag
     return '<h1>Hello World :)</h1>'
 
+@app.route('/greet')
+def greet():
+    return "Hello"
+
 if __name__ == '__main__':
     app.run()
+
+if __name__ == '__main__':
+    app.run()
+
