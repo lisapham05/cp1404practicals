bb4b9c02500cece5adb86ce936d8473ce3b27b7c
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-29 21:00:15 +0800

    create main function for asking user_search title, and print title, summary, url

---
+++ prac_10/wiki.py
@@ -0,0 +1,15 @@
+import wikipedia
+
+def main():
+    user_search = input("Enter page title: ")
+
+    while user_search:
+        try:
+            # Use auto_suggest=False to prevent automatic redirection
+            # and to trigger the specific errors for "python" and "jcu".
+            page = wikipedia.page(user_search, auto_suggest=False)
+
+            # For a successful result, print title, summary, and URL
+            print(page.title)
+            print(page.summary)
+            print(page.url)
