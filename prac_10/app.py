cb21db2028b5e538c6c5cd323a9b14a4b67784c6
lisapham05 <thixuanngoc.pham@my.jcu.edu.au>
2025-07-29 21:21:35 +0800

    add 2nd decorator for greet function

--- prac_10/app.py
+++ prac_10/app.py
@@ -8,8 +8,9 @@ def hello_world():
     return '<h1>Hello World :)</h1>'
 
 @app.route('/greet')
-def greet():
-    return "Hello"
+@app.route('/greet/<name>')
+def greet(name=""):
+    return f"Hello{name}"
 
 
 if __name__ == '__main__':
